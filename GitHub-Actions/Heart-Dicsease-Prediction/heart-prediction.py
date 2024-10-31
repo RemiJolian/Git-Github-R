f7bdebcbd819621604395fa79f0c4416c67e899b
RaminBagheri <102723506+RemiJolian@users.noreply.github.com>
2024-10-31 20:39:20 +0330

    Delete a comment in heart-prediction.py

--- GitHub-Actions/Heart-Dicsease-Prediction/heart-prediction.py
+++ GitHub-Actions/Heart-Dicsease-Prediction/heart-prediction.py
@@ -6,7 +6,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from xgboost import XGBClassifier
 import matplotlib.pyplot as plt
-#plt.style.use('./deeplearning.mplstyle')
 
 RANDOM_STATE = 55 ## We will pass it to every sklearn call so we ensure reproducibility
 
