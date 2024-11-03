32265a094d232895849067931f08dd6587c85502
RaminBagheri <rbagheri85@gmail.com>
2024-11-03 07:44:27 +0330

    d

--- GitHub-Actions/Heart-Dicsease-Prediction/heart-prediction.py
+++ GitHub-Actions/Heart-Dicsease-Prediction/heart-prediction.py
@@ -4,7 +4,7 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
-from xgboost import XGBClassifier
+#from xgboost import XGBClassifier
 import matplotlib.pyplot as plt
 #plt.style.use('./deeplearning.mplstyle')
 
